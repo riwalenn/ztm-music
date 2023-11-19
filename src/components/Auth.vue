50006013e9cb6ec731d6ed0d993322890405dc9e
Riwalenn Bas <riwalenn@gmail.com>
2023-11-19 17:12:22 +0100

    (MAJ) validations

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -6,7 +6,16 @@ export default {
   name: 'AppAuth',
   data() {
     return {
-      tab: 'login'
+      tab: 'login',
+      schema: {
+        name: 'required|min:3|max:100|alpha_spaces',
+        email: '',
+        age: '',
+        password: '',
+        confirm_password: '',
+        country: '',
+        tos: ''
+      }
     }
   },
   computed: {
@@ -101,15 +110,18 @@ export default {
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <!-- if, for example, it was a select field => vee-form as="select" / input is by default -->
+              <vee-field
+                name="name"
                 type="text"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -168,7 +180,7 @@ export default {
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VeeValidatePlugin from './includes/validation'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VeeValidatePlugin)
 
 app.mount('#app')
