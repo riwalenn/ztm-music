cea7590bc3cc5ea8ce3db036e34fd9189cf0da13
Riwalenn Bas <riwalenn@gmail.com>
2023-11-20 20:29:08 +0100

    (MAJ) setting up the login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      loginSchema: {
+        email: 'required|min:3|max:100|email',
+        password: 'required|min:12|max:22|excluded:password'
+      },
+      login_in_submission: false,
+      login_show_alert: false,
+      login_alert_variant: 'bg-blue-500',
+      login_alert_msg: 'Please wait! We are logging you in.'
+    }
+  },
+  methods: {
+    login(values) {
+      this.login_show_alert = true
+      this.login_in_submission = true
+      this.login_alert_variant = 'bg-blue-500'
+      this.login_alert_msg = 'Please wait! We are logging you in.'
+
+      this.login_alert_variant = 'bg-green-500'
+      this.login_alert_msg = 'Success! Your are now logged in.'
+
+      console.log(values)
+    }
+  }
+}
+</script>
+
+<template>
+  <div
+    class="text-white text-center font-bold p-4 mb-4"
+    v-if="login_show_alert"
+    :class="login_alert_variant"
+  >
+    {{ login_alert_msg }}
+  </div>
+  <vee-form :validation-schema="loginSchema" @submit="login">
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <vee-field
+        name="email"
+        type="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email"
+      />
+      <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+        <input
+          type="password"
+          class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          placeholder="Password"
+          v-bind="field"
+        />
+        <div class="text-red-600" v-for="error in errors" :key="error">
+          {{ error }}
+        </div>
+      </vee-field>
+      <ErrorMessage class="text-red-600" name="password" />
+    </div>
+    <button
+      type="submit"
+      :disabled="login_in_submission"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+    >
+      Submit
+    </button>
+  </vee-form>
+</template>
