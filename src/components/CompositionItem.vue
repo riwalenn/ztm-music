da9271af0d76248787578364e25168d01f4c3b1a
Riwalenn Bas <riwalenn@gmail.com>
2023-11-25 17:27:32 +0100

    (MAJ) editing a song

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -1,9 +1,19 @@
 <script>
+import { songsCollection } from '@/includes/firebase'
+
 export default {
   name: 'CompositionItem',
   props: {
     song: {
       type: Object,
+      required: true
+    },
+    updateSong: {
+      type: Function,
+      required: true
+    },
+    index: {
+      type: Number,
       required: true
     }
   },
@@ -13,11 +23,35 @@ export default {
       schema: {
         modified_name: 'required',
         genre: 'alpha_spaces'
-      }
+      },
+      in_submission: false,
+      show_alert: false,
+      alert_variant: 'bg-blue-500',
+      alert_message: 'Please wait! Updating song info.'
     }
   },
   methods: {
-    edit() {}
+    async edit(values) {
+      this.in_submission = true
+      this.show_alert = true
+      this.alert_variant = 'bg-blue-500'
+      this.alert_message = 'Please wait! Updating song info.'
+
+      try {
+        await songsCollection.doc(this.song.docID).update(values)
+      } catch (error) {
+        this.in_submission = false
+        this.alert_variant = 'bg-red-500'
+        this.alert_message = 'Something went wrong! Try again later.'
+        return
+      }
+
+      await this.updateSong(this.index, values)
+
+      this.in_submission = false
+      this.alert_variant = 'bg-green-500'
+      this.alert_message = 'Success !'
+    }
   }
 }
 </script>
@@ -37,6 +71,13 @@ export default {
       </button>
     </div>
     <div v-show="showForm">
+      <div
+        class="text-white text-center font-bold p-4 mb-4"
+        v-if="show_alert"
+        :class="alert_variant"
+      >
+        {{ alert_message }}
+      </div>
       <vee-form :validation-schema="schema" :initial-values="song" @submit="edit">
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
@@ -58,8 +99,21 @@ export default {
           />
           <ErrorMessage class="text-red-600" name="genre" />
         </div>
-        <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
-        <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">Go Back</button>
+        <button
+          type="submit"
+          class="py-1.5 px-3 rounded text-white bg-green-600"
+          :disabled="in_submission"
+        >
+          Submit
+        </button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+          :disabled="in_submission"
+          @click.prevent="showForm = false"
+        >
+          Go Back
+        </button>
       </vee-form>
     </div>
   </div>
