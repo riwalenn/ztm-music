f8adf13736559a4f7a30540bf751f955e7e094c0
Riwalenn Bas <riwalenn@gmail.com>
2023-11-25 13:58:50 +0100

    (MAJ) fallback upload

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -13,7 +13,7 @@ export default {
     upload($event) {
       this.is_dragover = false
 
-      const files = [...$event.dataTransfer.files]
+      const files = $event.dataTransfer ? [...$event.dataTransfer.files] : [...$event.target.files]
 
       files.forEach((file) => {
         if (file.type !== 'audio/mpeg') {
@@ -91,6 +91,7 @@ export default {
       >
         <h5>Drop your files here</h5>
       </div>
+      <input type="file" multiple @change="upload($event)" />
       <hr class="my-6" />
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
