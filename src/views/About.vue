80c9e52066f33324b2c39231c66b9a06868a3289
Riwalenn Bas <riwalenn@gmail.com>
2023-11-25 18:58:44 +0100

    (MAJ) Path parameters

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,8 @@
+<script>
+export default {
+  name: 'About'
+}
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
---
+++ src/views/Song.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'Song'
+}
+</script>
+
+<template>
+  <div>
+    {{ $route.params.id }}
+  </div>
+</template>
