f75ccc72e6014dbed84cb97df78867360ff3377f
Riwalenn Bas <riwalenn@gmail.com>
2023-11-26 18:47:18 +0100

    (MAJ) routes transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <app-header />
 
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 
   <app-player />
 
@@ -32,3 +36,17 @@ export default {
   }
 }
 </script>
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
