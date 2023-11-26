77a89ab5029db64477f83195f297544234663360
Riwalenn Bas <riwalenn@gmail.com>
2023-11-26 14:11:05 +0100

    (MAJ) playing audio

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,12 +1,21 @@
 import { defineStore } from 'pinia'
+import { Howl } from 'howler'
 
 export default defineStore('player', {
   actions: {
     state: () => ({
-      current_song: {}
+      current_song: {},
+      sound: {}
     }),
     async newSong(song) {
       this.current_song = song
+
+      this.sound = new Howl({
+        src: [song.url],
+        html5: true
+      })
+
+      this.sound.play()
     }
   }
 })
