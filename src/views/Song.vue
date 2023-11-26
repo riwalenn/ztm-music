c50ce03f27374d7b532b5f53808caaa12648f9c8
Riwalenn Bas <riwalenn@gmail.com>
2023-11-26 10:31:16 +0100

    (MAJ) finalizing the comment form

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -1,5 +1,7 @@
 <script>
-import { songsCollection } from '@/includes/firebase'
+import { songsCollection, auth, commentsCollection } from '@/includes/firebase'
+import { mapState } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'Song',
@@ -12,8 +14,11 @@ export default {
       comment_in_submission: false,
       comment_show_alert: false,
       comment_alert_variant: 'bg-blue-500',
-      comment_alert_message: 'Please wait ! Your comment is being submitted.'
+      comment_alert_message: 'Please wait! Your comment is being submitted.'
     }
+  },
+  computed: {
+    ...mapState(useUserStore, ['userLoggedIn'])
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get()
@@ -26,11 +31,27 @@ export default {
     this.song = docSnapshot.data()
   },
   methods: {
-    async addComment(values) {
+    async addComment(values, { resetForm }) {
       this.comment_in_submission = true
       this.comment_show_alert = true
       this.comment_alert_variant = 'bg-blue-500'
-      this.comment_alert_message = 'Please wait ! Your comment is being submitted.'
+      this.comment_alert_message = 'Please wait! Your comment is being submitted.'
+
+      const comment = {
+        content: values.comment,
+        datePosted: new Date().toString(),
+        sid: this.$route.params.id,
+        name: auth.currentUser.displayName,
+        uid: auth.currentUser.uid
+      }
+
+      await commentsCollection.add(comment)
+
+      this.comment_in_submission = false
+      this.comment_alert_variant = 'bg-green-500'
+      this.comment_alert_message = 'Comment added!'
+
+      resetForm()
     }
   }
 }
@@ -74,7 +95,7 @@ export default {
         >
           {{ comment_alert_message }}
         </div>
-        <vee-form :validation-schema="schema" @submit="addComment">
+        <vee-form :validation-schema="schema" @submit="addComment" v-if="userLoggedIn">
           <vee-field
             as="textarea"
             name="comment"
