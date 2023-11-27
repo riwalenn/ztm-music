17337f37346bdc5a0636386d64f42aa8e3fc77c0
Riwalenn Bas <riwalenn@gmail.com>
2023-11-27 21:11:45 +0100

    (MAJ) Changing locales

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,9 +6,15 @@ import useUserStore from '@/stores/user'
 export default {
   name: 'AppHeader',
   computed: {
-    ...mapStores(useModalStore, useUserStore)
+    ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === 'fr' ? 'French' : 'English'
+    }
   },
   methods: {
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === 'fr' ? 'en' : 'fr'
+    },
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
       console.log(this.modalStore.isOpen)
@@ -56,6 +62,13 @@ export default {
               <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
             </li>
           </template>
+        </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">{{
+              currentLocale
+            }}</a>
+          </li>
         </ul>
       </div>
     </nav>
