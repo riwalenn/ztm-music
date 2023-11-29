6e3f94e399ee46fc248aba8377add2af48eb7a37
Riwalenn Bas <riwalenn@gmail.com>
2023-11-29 19:09:53 +0100

    (MAJ) Firebase Data Persistence

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -18,6 +18,11 @@ firebase.initializeApp(firebaseConfig)
 const auth = firebase.auth()
 const db = firebase.firestore()
 const storage = firebase.storage()
+
+db.enablePersistence().catch((error) => {
+  console.error(`Firebase persistence error ${error.code}`, error)
+})
+
 const usersCollection = db.collection('users')
 const songsCollection = db.collection('songs')
 const commentsCollection = db.collection('comments')
